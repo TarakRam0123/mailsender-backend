faafdfb0053c103d7eb8a4ab2bfc61cb1102b672
TarakRam0123 <taraka.ram0123@gmail.com>
2025-12-23 16:11:16 +0530

    issue fixed in upload middleware

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -3,4 +3,4 @@ const multer = require("multer");
 const storage = multer.memoryStorage(); // use memory storage for uploading to S3
 const upload = multer({ storage });
 
-export default upload;
+module.exports = upload;
